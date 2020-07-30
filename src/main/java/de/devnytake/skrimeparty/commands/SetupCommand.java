f26ccde947505172d6e8c4d34fd3ad2f91563915
DasSpigotSystem <max.luehring1@gmail.com>
2020-07-30 18:32:45 +0200

    PartyGames

--- src/main/java/de/devnytake/skrimeparty/commands/SetupCommand.java
+++ src/main/java/de/devnytake/skrimeparty/commands/SetupCommand.java
@@ -1,7 +1,6 @@
 package de.devnytake.skrimeparty.commands;
 
 import de.devnytake.skrimeparty.PartyGames;
-import de.devnytake.skrimeparty.games.Games;
 import de.devnytake.skrimeparty.map.Map;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
@@ -18,19 +17,19 @@ public class SetupCommand implements CommandExecutor {
 
     private PartyGames plugin;
 
-    public SetupCommand(PartyGames plugin){
+    public SetupCommand(PartyGames plugin) {
         this.plugin = plugin;
         plugin.getCommand("setup").setExecutor(this);
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String s, String[] args) {
-        if(!(sender instanceof Player)) {
+        if (!(sender instanceof Player)) {
             Bukkit.getConsoleSender().sendMessage("§cDazu musst du ein Spieler sein!");
             return false;
         }
         if(!cmd.getName().equals("setup"))return false;
         Player p = (Player)sender;
-        if(p.hasPermission("SkrimeParty.commands.setup")){
+        if(p.hasPermission("PartyGames.commands.setup")){
             if(args.length == 0){
                 p.sendMessage("---------§9Setup-----------");
                 p.sendMessage("");
@@ -38,21 +37,20 @@ public class SetupCommand implements CommandExecutor {
                 p.sendMessage("§9/setup §esetspawn §7<SpielModi> <Map-Name>");
                 p.sendMessage("");
                 p.sendMessage("---------§9Setup-----------");
-            }else{
-                    if (args[0].equalsIgnoreCase("create")) {
-                        Map map = new Map(args[1], args[2]);
-                        if (!map.exists()) {
-                            map.create(args[3]);
-                        }
-                    }else if(args[0].equalsIgnoreCase("setspawn")){
-                        Map map = new Map(args[1], args[2]);
-                        if(map.exists()){
-                            map.setSpawn(p.getLocation());
-                        }
+            } else {
+                if (args[0].equalsIgnoreCase("create")) {
+                    Map map = new Map(args[1], args[2]);
+                    if (!map.exists()) {
+                        map.create(args[3]);
                     }
-
+                } else if (args[0].equalsIgnoreCase("setspawn")) {
+                    Map map = new Map(args[1], args[2]);
+                    if (map.exists()) {
+                        map.setSpawn(p.getLocation());
+                    }
+                }
             }
-        }else
+        } else
             p.sendMessage(plugin.getPrefix() + plugin.getNoPerm());
         return false;
     }
